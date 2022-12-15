552f7a302a9ccf50cf31d9e4a4900ab0687dbabc
dev_kranthi <kiran.minna1999@gmail.com>
2022-12-15 20:59:12 +0530

    wdwd

--- server/index.js
+++ server/index.js
@@ -1,36 +1,18 @@
-const app = require("express")();
 const express = require("express");
-const http = require("http").createServer(app);
-const cors = require("cors");
-app.use(express.json());
-app.use(
-  cors({
-    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-  })
-);
-const mongoose = require("mongoose");
 
-//database connection
-mongoose
-  .connect(
-    "mongodb+srv://newuser:newuser@socialmedia.emihq.mongodb.net/chat_app?retryWrites=true&w=majority",
-    {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    }
-  )
-  .then(() => {
-    console.log("database connected");
-  })
-  .catch((err) => {
-    console.log("db not connected", err);
-  });
+const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hey Socket.io</h1>");
+  res.send("Express on Vercel");
 });
-//http requests
 
-http.listen(3000, () => {
-  console.log("listening on *:3000");
+app.get("/api/users", (req, res) => {
+  res.status(200).send({ message: "user saved successfully" });
 });
+
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
+
+// Export the Express API
+module.exports = app;
