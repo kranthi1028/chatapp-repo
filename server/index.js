f3ddf15c491b657160f47c1645fe12d96375f01d
dev_kranthi <kiran.minna1999@gmail.com>
2022-12-15 21:02:32 +0530

    efe

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
-
 const app = express();
-
+const cors = require("cors");
+app.use(express.json());
+app.use(
+  cors({
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  })
+);
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
